73731099fe6c88d3c73208555dc6dc3bc02b7bdf
LautaroGonzalez159 <lautarogonzalezsanchez1@gmail.com>
2022-07-02 22:15:43 -0300

    Web Site 0.3

--- JS/baseDeDatos.js
+++ JS/baseDeDatos.js
@@ -1,16 +1,16 @@
-const tipoOp =[{"tipo": "Peso Argentino","Factor": 0.45,"MON":"ARS"},
-               {"tipo": "DOLAR","Factor": 0.23,"MON":"USD"},
-               {"tipo": "EURO","Factor": 0.19,"MON":"EUR"},]
+const tipoOp =[{tipo: "Peso Argentino",Factor: 0.45,"MON":"ARS"},
+               {tipo: "DOLAR",Factor: 0.23,"MON":"USD"},
+               {tipo: "EURO",Factor: 0.19,"MON":"EUR"},]
                
-const plazoOp =[{"tiempo": "30","factor":1},
-                {"tiempo": "60","factor":2},
-                {"tiempo": "90","factor":3},
-                {"tiempo": "120","factor":4},]
+const plazoOp =[{tiempo: "30",factor:1},
+                {tiempo: "60",factor:2},
+                {tiempo: "90",factor:3},
+                {tiempo: "120",factor:4},]
 
-const montoOpe = [{"monto": "10000", "factor": 10000},
-                  {"monto": "20000", "factor": 20000},
-                  {"monto": "30000", "factor": 30000},
-                  {"monto": "40000", "factor": 40000},
-                  {"monto": "50000", "factor": 50000},]
+const montoOpe = [{monto: "10000", "factor": 10000},
+                  {monto: "20000", "factor": 20000},
+                  {monto: "30000", "factor": 30000},
+                  {monto: "40000", "factor": 40000},
+                  {monto: "50000", "factor": 50000},]
 
 const cajeros = ["Luis","Lautaro","Emiliano","Sofia"]
